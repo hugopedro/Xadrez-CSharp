40a12f9553df92d16fd0d803bd96fa7fd2a0d171
Hugo Pedro <hugopedro098@gmail.com>
2019-02-15 11:36:35 -0200

    Começando a implementar um método que imprime o tabuleiro

--- xadrez-treino/tabuleiro/Tabuleiro.cs
+++ xadrez-treino/tabuleiro/Tabuleiro.cs
@@ -2,7 +2,7 @@
 {
     class Tabuleiro
     {
-        public int linhas { get; set; }
+        public int linhas { get; set; } 
         public int colunas { get; set; }
         // o jogo de xadrez tambem terá uma matriz de peças
         private Peca[,] pecas; //private pq nao pode ser acessados por alguem de fora
@@ -14,6 +14,12 @@
             pecas = new Peca[linhas, colunas]; // Instanciando a matriz de peças
             // as peças irão receber uma nova matriz de peças que vai receber as linhas e colunas
             //passadas no argumento, portanto não entra diretamente no argumento
+        }
+
+        // método para usar no tabuleiro para colocar a peça no tabuleiro, (encapsulamento)
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
         }
     }
 }
