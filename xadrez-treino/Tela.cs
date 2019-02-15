106914dcadad1182a65116dedbb6210c34a6d40d
Hugo Pedro <hugopedro098@gmail.com>
2019-02-15 21:45:35 -0200

    Melhorando a implementacao do metodo imprimirTabuleiro

--- xadrez-treino/Tela.cs
+++ xadrez-treino/Tela.cs
@@ -1,27 +1,48 @@
 ﻿using System;
 using tabuleiro;
 
-namespace xadrez_treino
+namespace tabuleiro
 {
     class Tela
     {
-        //Começando a implementar um método que imprime o tabuleiro
+
         public static void imprimirTabuleiro(Tabuleiro tab)
         {
-            for (int i =0; i<tab.linhas; i++)
+
+            for (int i = 0; i < tab.linhas; i++)
             {
-                for (int j=0; j<tab.colunas; j++)
-                { // se a peça na posicao i,j for nula nao tem peça, entao imprimo o Tracinho - 
-                    if (tab.peca(i,j) == null)
+                Console.Write(8 - i + " ");
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (tab.peca(i, j) == null)
                     {
                         Console.Write("- ");
-                    } else
+                    }
+                    else // imprime a peça e depois um espaçinho em branco
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h");
         }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            { //aqui ele joga a cor cinza do console numa variavel de sistema aux
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow; //obriga a cor cinza do console virar amarelo
+                Console.Write(peca); // escreve a peça amarela
+                Console.ForegroundColor = aux; // devolve o valor cinza padrão novamente
+            }
+        }
+
     }
 }
